a6b70f361242b502f60143e8f819a688f0d1bfca
SNP500INDEX <nahs92@naver.com>
2019-07-30 09:10:37 +0900

    great!

---
+++ src/java_20190729/InterA.java
@@ -0,0 +1,12 @@
+package java_20190729;
+
+//인터페이스
+
+public interface InterA {
+	//인터페이스의 변수 public static final 이 생략되어 있음.
+	public static final double PI = 3.14;
+	//인터페이스 메서드의 접근 한정자를 붙이지 않으면 public 이 생략되어 있음
+	//abstract 생략 가능.
+	public abstract void mA();
+
+}
